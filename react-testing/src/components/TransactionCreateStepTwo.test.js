ff5ebbbd209dab639c11f5424b5dd0818f02f63a
kalio <kalioprince1@gmail.com>
2023-07-12 18:24:17 +0000

    wrote a unit test for transactionCreateStepTow

---
+++ react-testing/src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+
+test("on intial render the pay button is disabled", () => {
+
+    render(<TransactionCreateStepTwo sender={{id:5}} receiver={{id:2}} />);
+    screen.debug();
+    
+})
+// screen.getRole(); this helps with knowing the particular function of each div
