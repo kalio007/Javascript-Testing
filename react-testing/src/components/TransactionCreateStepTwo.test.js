b3de200375d014ae47f3f733e40c3bc665afc0f5
kalio <kalioprince1@gmail.com>
2023-07-13 09:17:32 +0000

    wrote a unit test for transactionCreateStepTwo

--- react-testing/src/components/TransactionCreateStepTwo.test.js
+++ react-testing/src/components/TransactionCreateStepTwo.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
-test("on intial render the pay button is disabled", () => {
+test("on intial render the pay button is deactivated", () => {
 
     render(<TransactionCreateStepTwo sender={{id:5}} receiver={{id:2}} />);
     screen.debug();
